4d2de7f909b6f5ca01be31bf53a99c5e0f9ee921
James Kassemi <jkassemi@gmail.com>
2014-02-12 11:43:34 -0700

    More spec, back to more pressing matters

--- spec/registry_spec.rb
+++ spec/registry_spec.rb
@@ -1,17 +1,26 @@
 require 'spec_helper'
 
 describe Stubby::Registry do
+  subject { 
+    described_class.new 
+  }
+
+  before { 
+    stub_request(:get, /github\.com.*/).
+      to_return(body: Oj.dump({
+        "example" => {
+          "v1.0.0" => "http://example.com/stubby.zip",
+          "v2.0.0" => "http://example.com/stubby2.zip"
+        }
+      }), status: 200)
+  }
+
   describe "#index" do
-    subject { described_class.new }
-
     context "when remote index available" do
-      before { stub_request(:get, /github\.com.*/).
-                to_return(body: "{\"example2\": { \"v1.0.0\":\"http://example.com/stubby.zip\" }}", status:200) }
-
       it "provides a name => RegistryItem hash" do
-        expect(subject.index).to have_key("example2")
-        expect(subject.index["example2"]).to be_instance_of(Array)
-        expect(subject.index["example2"].first).to be_instance_of(Stubby::RegistryItem)
+        expect(subject.index).to have_key("example")
+        expect(subject.index["example"]).to be_instance_of(Array)
+        expect(subject.index["example"].first).to be_instance_of(Stubby::RegistryItem)
       end
     end
 
@@ -28,7 +37,19 @@ describe Stubby::Registry do
   end
 
   describe "#versions" do
+    context "when stub found" do
+      it "gives a sorted list of version" do
+        expect(subject.versions("example")).to be_instance_of(Array)
+        expect(subject.versions("example").length).to eq(2)
+        expect(subject.versions("example").first.version).to eq("2.0.0")
+      end      
+    end
 
+    context "when stub not found" do
+      it "gives an empty array" do
+        expect(subject.versions("nope")).to eq([])
+      end
+    end
   end
 
   describe "#latest" do
