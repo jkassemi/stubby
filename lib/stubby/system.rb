5cf223f9cc954611558d54dfdcd5391833aa5fab
James Kassemi <jkassemi@gmail.com>
2014-02-17 13:21:29 -0700

    Alternate states with session subcommand. Fixes #7

--- lib/stubby/system.rb
+++ lib/stubby/system.rb
@@ -1,14 +1,18 @@
 require 'stubby/stub'
 require 'listen'
+require 'fileutils'
 
 module Stubby
   class System
+    attr_accessor :session_name,
+      :root_path
+
     def initialize
       stubs
     end
 
     def dump
-      File.write(path, Oj.dump(Hash[@stubs.collect { |k,v|
+      File.write(session_config_path, Oj.dump(Hash[@stubs.collect { |k,v|
         [k, v.target] 
       }]))
     end
@@ -35,22 +39,56 @@ module Stubby
       dump
     end
 
-    def path
-      # TODO: configurable
-      File.expand_path("~/.stubby/system.json")
+    def session_name
+      @session_name || current_session_name
     end
 
-    def search_paths
-      # TODO: configurable
-      [File.expand_path("~/.stubby")]
+    def session_name=(name)
+      if name.to_s.empty?
+        name = "default"
+      end
+
+      if current_session_name != name
+        @session_name = name and File.write(session_name_path, session_name) and reload
+      end
+    end
+
+    def session_remove(name)
+      `rm #{session_root_path}/#{name}.json`
+    end
+
+    def root_path
+      @root_path || File.expand_path("~/.stubby")
+    end
+
+    def session_root_path
+      "#{root_path}/sessions".tap do |path|
+        FileUtils.mkdir_p(path) unless File.exists?(path)
+      end
+    end
+
+    # Stores the session values itself
+    def session_config_path
+      File.expand_path("#{session_root_path}/#{session_name}.json")
+    end
+
+    # Stores the current session name
+    def session_name_path
+      "#{session_root_path}/current"  
+    end
+
+    def current_session_name
+      if File.exists?(session_name_path)
+        File.read(session_name_path).strip
+      else
+        "default"
+      end
     end
 
     private
     def installed_stubs
       # TODO: clean me
-      Hash[search_paths.collect { |search_path|
-        Dir[search_path + "/**"]
-      }.flatten.collect { |path|
+      Hash[Dir[root_path + "/**"].collect { |path|
         next unless File.exists?(path + "/stubby.json")
         [File.basename(path), Stub.new(path + "/stubby.json")]
       }.compact]
@@ -60,7 +98,7 @@ module Stubby
       # TODO: clean me
       is = installed_stubs
 
-      if File.exists?(path)
+      if File.exists?(session_config_path)
         Hash[Oj.load(File.read(path)).collect { |k, v|
           is[k].target = v
           [k, is[k]]
